ce3793df98caf44afa3c664e46e93049bf4453dc
Kyle Mann <mannkyleca@gmail.com>
2015-01-15 16:06:23 -0500

    initial commit
    first submission for site - under construction

---
+++ myscript.js
@@ -0,0 +1,64 @@
+// Do a couple of console.logs
+//console.log("Hello from myscript.js");
+//console.log("Hello again!"); // this is not needed
+
+//
+//var name = prompt("What is your name?");
+//alert("Hello " + name);
+//
+//name = "Nick";
+//console.log("The user's name is "+ name);
+//
+
+//console.log("Before");
+//
+//var name = prompt("What is your name")
+//
+//if (name) {
+// console.log("If block"); 
+//} else {
+//    console.log("Else Block");
+//}
+//
+//
+//
+//
+//console.log("After");
+//
+//console.log("Before");
+//
+//var counter = 10;
+//
+//while (counter) {
+//console.log("Hello World!");
+//    counter = counter -1;
+//}
+//
+//while (prompt("what is your name?")) {
+//    console.log("got your name")
+//}
+//
+//console.log("After");
+//
+//console.log("After");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
